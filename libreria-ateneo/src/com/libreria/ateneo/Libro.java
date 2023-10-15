521fe0bb5feb6d539d169a7fd5167bbc12494b52
FelipeCSDav <nitruxbite@gmail.com>
2023-10-15 20:35:15 -0300

    Se crea clase libro y atributos

---
+++ libreria-ateneo/src/com/libreria/ateneo/Libro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.libreria.ateneo;
+
+/**
+ *
+ * @author Near
+ */
+public class Libro {
+//<editor-fold defaultstate="collapsed" desc="Atributos">
+    private String titulo,autor,isbn;
+    private int precio;
+//</editor-fold>
+}
