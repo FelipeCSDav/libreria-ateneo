8f6f295070f7a25d343b42f06d8bb46e0687d850
FelipeCSDav <nitruxbite@gmail.com>
2023-10-16 13:50:01 -0300

    Se agregaron mas cosas

--- libreria-ateneo/src/com/libreria/ateneo/Catalogo.java
+++ libreria-ateneo/src/com/libreria/ateneo/Catalogo.java
@@ -4,10 +4,41 @@
  */
 package com.libreria.ateneo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Near
  */
 public class Catalogo {
+   private ArrayList<Libro> catalogo;
+
+    public Catalogo() {
+        this.catalogo = new ArrayList<>();
+    }
+ 
+    //Agregar libros
+    public void agregaLibros(Libro libro){
+        this.catalogo.add(libro);
+        System.out.println("Libro agregado");
+    }
+    
+    //Mostrar libros
+    public void mostrarCatalogo(){
+        for (Libro libro : catalogo) {
+            libro.mostrarInfo();
+        }
+    }
+    
+    public void eliminarLibro(String isbn){
+        for (Libro libro : catalogo) {
+            if (libro.getIsbn().equalsIgnoreCase(isbn)) {
+                catalogo.remove(libro);
+                System.out.println("Libro: "+ isbn + " eliminado con exito");
+            }else{
+                System.out.println("Error al eliminar libro");
+            }
+        }
+    }
    
 }
