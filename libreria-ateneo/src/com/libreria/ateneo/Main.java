d31f8daf6fe5e3aa9c5629cb95e2718efeb0a12a
FelipeCSDav <nitruxbite@gmail.com>
2023-10-16 15:11:06 -0300

    Se agregaron mas funciones

--- libreria-ateneo/src/com/libreria/ateneo/Main.java
+++ libreria-ateneo/src/com/libreria/ateneo/Main.java
@@ -10,6 +10,28 @@ package com.libreria.ateneo;
  */
 public class Main {
     public static void main(String[] args) {
+        Autor mc = new Autor("Miguel", "Cervantes", "Caribeño");
+        Autor jk = new Autor ("JK", "Rowling", "Britanica");
+        Autor gm = new Autor("Gabriel", "Prado", "Peruano");
+        Libro harry = new Libro("HarryPotter", jk, "1234", 50000);
+        Libro cien = new Libro("Cien agnos", gm, "5468", 35000);
+        Libro quijote = new Libro("Don quijote", mc, "4644", 15000);
+        Catalogo cat = new Catalogo();
+        
+        //Agregar Libros
+        cat.agregaLibros(harry);
+        cat.agregaLibros(cien);
+        cat.agregaLibros(quijote);
+        System.out.println("");
+        //Mostrar coleccion de libros
+        cat.mostrarCatalogo();
+        System.out.println("----------------------------------------------");
+        //Eliminar libro
+        cat.eliminarLibro("4644");
+        //Volvemos a mostrar catalogo con el libro eliminado
+        cat.mostrarCatalogo();
+        //Buscar libro por apellido
+        cat.buscarLibroApellido("Rowling");
         
     }
 }
