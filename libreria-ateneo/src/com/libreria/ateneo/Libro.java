83b27dee59ff070e5490fe9dfbc39f84ed773b58
FelipeCSDav <nitruxbite@gmail.com>
2023-10-16 15:17:34 -0300

    Se agregaron mas funciones 2

--- libreria-ateneo/src/com/libreria/ateneo/Libro.java
+++ libreria-ateneo/src/com/libreria/ateneo/Libro.java
@@ -9,20 +9,61 @@ package com.libreria.ateneo;
  * @author Near
  */
 public class Libro {
-    
-    private String titulo,autor,isbn;
+//Atributos
+    private String titulo,isbn;
     private int precio;
-
+    private Autor autor;
+    
+//Constructores
     public Libro() {
     }
-    
-    
-
-    public Libro(String titulo, String autor, String isbn, int precio) {
+    public Libro(String titulo, Autor autor, String isbn, int precio) {
         this.titulo = titulo;
         this.autor = autor;
         this.isbn = isbn;
         this.precio = precio;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    //CUSTOMERS
+    public void mostrarInfo(){
+        System.out.println("Titulo: "+this.titulo + "\nAutor: "+this.autor + "\nISBN: "+this.isbn + "\nPrecio: $"+this.precio);
+    }
+    
+    public double calcularIva(){
+        double IVA= 0.19;
+        return this.precio * IVA;
     }
     
     
